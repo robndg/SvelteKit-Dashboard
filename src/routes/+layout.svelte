5946879c345e6305dae9f83c937305cb08078b09
robndg <37436428+robndg@users.noreply.github.com>
2023-02-02 14:08:54 -0800

    feat: Add column layout and started API price calc

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,21 @@
 </script>
 <div class="min-h-full">
   <TopBar/>
-  <slot />
+  <main class="flex-1 p-4">
+    <!-- Column Layout -->
+    <div class="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
+      <!-- Conent Left (SideBar Menu) -->
+      <div class="mt-4 lg:row-span-3 lg:mt-0">
+        
+        
+      </div>
+
+      <!-- Content Right (Yield Page Slot)-->
+      <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8">
+        <slot />
+
+      </div>
+    </div>
+    
+  </main>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,62 @@
 
+<script type="ts">
+  import { onMount } from 'svelte';
 
+  // User Settings
+  let currency = "USD";
+  // Selected User Wallet Data
+  let cryptoFull = "bitcoin";
+  const balance = 0.4;
+  // Selected Price History
+  let days = 1;
+  // Price History
+  let pricePrevious;
+  let priceHistory: any;
+  // Displayed Price Data
+  let priceToday: any;
+  let percentageIncrease: any;
 
-  <style lang="scss">
-    div {
-      p {
-        color: red;
-      }
-    }
-  </style>
+  /* Test: Use <User Settings> and <Selected Wallet Data> with fetched <PriceHistory> */
+
+  // Invoke this function when the component is mounted to the DOM
+  onMount(async () => {
+    // Fetch the historical close price data from the API
+    const resHistoricalClose = await fetch(`https://api.coingecko.com/api/v3/coins/${cryptoFull}/market_chart?vs_currency=${currency}&days=${days}&interval=hourly`);
+    priceHistory = await resHistoricalClose.json();
+    console.log(priceHistory);
+
+    // Calculate the number of hours that correspond to the number of days specified
+    let daysToHours = (days * 24) - 1;
+    
+    // Get the price from the specified number of days ago
+    pricePrevious = priceHistory.prices[daysToHours][1];
+    console.log(pricePrevious);
+
+    // Get the current price
+    priceToday = priceHistory.prices[0][1];
+    console.log(priceHistory.prices[0][1]);
+
+    // Convert the prices to floating point numbers for calculation
+    let today = parseFloat(priceToday);
+    let previous = parseFloat(pricePrevious);
+
+    // Calculate the increase in price as a percentage (TODO: include changes inclusive of balance)
+    let increase = (today - previous) / previous;
+    let valueIncrease = balance * increase;
+    percentageIncrease = valueIncrease / balance * 100;
+
+  });
+  
+  // TODO: structure of the response data (real-world scenario, more specified)
+  type CurrencyData = {
+    price: { [key: string]: { priceToday: number } }
+  }
+
+  let dataCur: CurrencyData;
+
+</script>
+
+{#if priceHistory}
+  <p>{currency}: {priceToday}</p>
+  <p>{percentageIncrease}</p>
+{/if}
